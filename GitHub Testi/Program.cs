ba81722da96b76eac9be45d368daf354a24d1468
henri <henrihavimo@hotmail.com>
2021-09-14 19:00:53 +0300

    Add project files.

---
+++ GitHub Testi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHub_Testi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Terve!");
+        }
+    }
+}
